96164fbfba2da5d92c2dd848b6322a1e015c112f
DongHeon Kim <anicookie@naver.com>
2012-02-03 18:22:43 +0900

    Modifying For Mach

--- products/cyanogen_lu3000.mk
+++ products/cyanogen_lu3000.mk
@@ -18,10 +18,10 @@ PRODUCT_MANUFACTURER := LGE
 PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=lge_bproj BUILD_ID=GRJ22 BUILD_FINGERPRINT="lge/lge_bproj/bproj_EUR-XXX:2.3.4/GRJ22/LG-lu3000-V20d.421CC761:user/release-keys" PRIVATE_BUILD_DESC="lge_bproj-user 2.3.4 GRJ22 LG-lu3000-V20d.421CC761 release-keys"
 
 # Broadcom FM radio
-$(call inherit-product, vendor/cyanogen/products/bcm_fm_radio.mk)
+#$(call inherit-product, vendor/cyanogen/products/bcm_fm_radio.mk)
 
 # Release name and versioning
-PRODUCT_RELEASE_NAME := OptimusBlack
+PRODUCT_RELEASE_NAME := OptimusMACH
 PRODUCT_VERSION_DEVICE_SPECIFIC :=
 -include vendor/cyanogen/products/common_versions.mk
 
